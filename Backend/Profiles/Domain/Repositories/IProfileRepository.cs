bbc13164b7cf8fcb5d4e9a5aef872decf35c888d
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-06-21 01:39:43 -0500

    Feat: Updated the Profile Repository Interface
    In this part we updated the profile repository interface

--- Backend/Profiles/Domain/Repositories/IProfileRepository.cs
+++ Backend/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -1,6 +1,10 @@
+using Backend.Profiles.Domain.Model.Aggregates;
+using Backend.Profiles.Domain.Model.ValueObjects;
+using Backend.Shared.Domain.Repositories;
+
 namespace Backend.Profiles.Domain.Repositories;
 
-public interface IProfileRepository
+public interface IProfileRepository : IBaseRepository<Profile>
 {
-    
+    Task<Profile?> FindProfileByEmailAsync(EmailAddress email);
 }
