180d6e5eebbb006a7188b1788c3b7726a006c182
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-06-21 02:22:26 -0500

    Feat: Updated AIM ACL Services
    In this part we updated the services from the AIM ACL

--- Backend/IAM/Interfaces/ACL/Services/IamContextFacade.cs
+++ Backend/IAM/Interfaces/ACL/Services/IamContextFacade.cs
@@ -1,6 +1,31 @@
+using Backend.IAM.Domain.Model.Commands;
+using Backend.IAM.Domain.Model.Queries;
+using Backend.IAM.Domain.Services;
+
 namespace Backend.IAM.Interfaces.ACL.Services;
 
-public class IamContextFacade
+public class IamContextFacade(IUserCommandService userCommandService, IUserQueryService userQueryService) : IIamContextFacade
 {
-    
+    public async Task<int> CreateUser(string email, string password)
+    {
+        var signUpCommand = new SignUpCommand(email, password);
+        await userCommandService.Handle(signUpCommand);
+        var getUserByEmailQuery = new GetUserByEmailQuery(email);
+        var result = await userQueryService.Handle(getUserByEmailQuery);
+        return result?.Id ?? 0;
+    }
+
+    public async Task<int> FetchUserIdByEmail(string email)
+    {
+        var getUserByEmailQuery = new GetUserByEmailQuery(email);
+        var result = await userQueryService.Handle(getUserByEmailQuery);
+        return result?.Id ?? 0;
+    }
+
+    public async Task<string> FetchEmailByUserId(int userId)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var result = await userQueryService.Handle(getUserByIdQuery);
+        return result?.Email ?? string.Empty;
+    }
 }
