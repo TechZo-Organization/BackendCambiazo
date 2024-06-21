ad7a663712aa3b522a7d3a9cd2b6c7eb0e17aa54
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-06-21 01:46:04 -0500

    Feat: Updated the ACL Interfaces Profiles
    In this part we updated the ACL Interfaces classes

--- Backend/Profiles/Interfaces/ACL/IProfilesContextFacade.cs
+++ Backend/Profiles/Interfaces/ACL/IProfilesContextFacade.cs
@@ -2,5 +2,7 @@ namespace Backend.Profiles.Interfaces.ACL;
 
 public interface IProfilesContextFacade
 {
-    
+    Task<int> CreateProfile(string firstName, string lastName, string email, int phone, string photo);
+
+    Task<int> FetchProfileIdByEmail(string email);
 }
