6b905d529d385a6bd8663628becc9299f4896c41
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-06-21 02:05:54 -0500

    Feat: Updated User Repository
    In this part we updated the user repository

--- Backend/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
+++ Backend/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -1,6 +1,20 @@
+using Backend.IAM.Domain.Model.Aggregates;
+using Backend.IAM.Domain.Repositories;
+using Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace Backend.IAM.Infrastructure.Persistence.EFC.Repositories;
 
-public class UserRepository
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
 {
-    
+    public async Task<User?> FindByEmailAsync(string email)
+    {
+        return await Context.Set<User>().FirstOrDefaultAsync(user => user.Email.Equals(email));
+    }
+
+    public bool ExistsByEmail(string email)
+    {
+        return Context.Set<User>().Any(user => user.Email.Equals(email));
+    }
 }
