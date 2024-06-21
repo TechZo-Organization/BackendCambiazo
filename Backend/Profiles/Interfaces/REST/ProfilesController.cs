f3e195a7fc11813ef1cb4a632302ec5a56ae864f
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-06-21 01:51:40 -0500

    Feat: Updated Profile Controller class
    In this part we updated the profile controller class

--- Backend/Profiles/Interfaces/REST/ProfilesController.cs
+++ Backend/Profiles/Interfaces/REST/ProfilesController.cs
@@ -1,6 +1,43 @@
+using System.Net.Mime;
+using Backend.Profiles.Domain.Model.Queries;
+using Backend.Profiles.Domain.Services;
+using Backend.Profiles.Interfaces.REST.Resources;
+using Backend.Profiles.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Backend.Profiles.Interfaces.REST;
 
-public class ProfilesController
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class ProfilesController(IProfileCommandService profileCommandService, IProfileQueryService profileQueryService) : ControllerBase
 {
-    
+    [HttpPost]
+    public async Task<IActionResult> CreateProfile(CreateProfileResource resource)
+    {
+        var createProfileCommand = CreateProfileCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var profile = await profileCommandService.Handle(createProfileCommand);
+        if (profile is null) return BadRequest();
+        var profileResource = ProfileResourceFromEntityAssembler.ToResourceFromEntity(profile);
+        return CreatedAtAction(nameof(GetProfileById), new { profileId = profileResource.Id }, profileResource);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllProfiles()
+    {
+        var getAllProfilesQuery = new GetAllProfilesQuery();
+        var profiles = await profileQueryService.Handle(getAllProfilesQuery);
+        var profileResources = profiles.Select(ProfileResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(profileResources);
+    }
+
+    [HttpGet("{profileId:int}")]
+    public async Task<IActionResult> GetProfileById(int profileId)
+    {
+        var getProfileByIdQuery = new GetProfileByIdQuery(profileId);
+        var profile = await profileQueryService.Handle(getProfileByIdQuery);
+        if (profile == null) return NotFound();
+        var profileResource = ProfileResourceFromEntityAssembler.ToResourceFromEntity(profile);
+        return Ok(profileResource);
+    }
 }
