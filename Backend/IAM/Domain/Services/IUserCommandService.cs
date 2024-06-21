e9b1692b76b7477081447515bec265a84c732487
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-06-21 01:59:32 -0500

    Feat: Updated IAM Domain Services
    In this part we updated the IAM Domain Services

--- Backend/IAM/Domain/Services/IUserCommandService.cs
+++ Backend/IAM/Domain/Services/IUserCommandService.cs
@@ -1,6 +1,11 @@
+using Backend.IAM.Domain.Model.Aggregates;
+using Backend.IAM.Domain.Model.Commands;
+
 namespace Backend.IAM.Domain.Services;
 
 public interface IUserCommandService
 {
-    
+    Task<(User user, string token)> Handle(SignInCommand command);
+
+    Task Handle(SignUpCommand command);
 }
--- Backend/IAM/Domain/Services/IUserQueryService.cs
+++ Backend/IAM/Domain/Services/IUserQueryService.cs
@@ -1,6 +1,13 @@
+using Backend.IAM.Domain.Model.Aggregates;
+using Backend.IAM.Domain.Model.Queries;
+
 namespace Backend.IAM.Domain.Services;
 
 public interface IUserQueryService
 {
-    
+    Task<User?> Handle(GetUserByIdQuery query);
+
+    Task<IEnumerable<User>> Handle(GetAllUsersQuery query);
+
+    Task<User?> Handle(GetUserByEmailQuery query);
 }
