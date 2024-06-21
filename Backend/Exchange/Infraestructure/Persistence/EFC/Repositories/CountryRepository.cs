d126f9592a46d4291d127463520b7ed0e8f03385
123-Mathias <u202216282@upc.edu.pe>
2024-06-21 09:09:06 -0500

    Feat: Location Repository
    In this part, we created Country, Department and District Repository.

---
+++ Backend/Exchange/Infraestructure/Persistence/EFC/Repositories/CountryRepository.cs
@@ -0,0 +1,16 @@
+using Backend.Exchange.Domain.Model.Enitities;
+using Backend.Exchange.Domain.Repositories;
+using Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Exchange.Infraestructure.Persistence.EFC.Repositories;
+
+public class CountryRepository(AppDbContext context) : BaseRepository<Country>(context) , ICountryRepository
+{
+    public async Task<Country?> GetByNameAsync(string name)
+    {
+        return await context.Set<Country>()
+            .FirstOrDefaultAsync(c => c.Name == name);
+    }
+}
