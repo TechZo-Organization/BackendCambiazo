bd28bfbe55caeeb87f2ff9d7acb675b971325bef
123-Mathias <u202216282@upc.edu.pe>
2024-06-21 09:18:24 -0500

    Feat: Location Controllers
    In this part we created Country Controller, District Controller, and Department Controller.

---
+++ Backend/Exchange/Interfaces/REST/CountryController.cs
@@ -0,0 +1,48 @@
+using Backend.Exchange.Domain.Model.Commnads.CountryCommands;
+using Backend.Exchange.Domain.Services;
+using Backend.Exchange.Interfaces.REST.Resources;
+using Backend.Exchange.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Exchange.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CountryController(ICountryCommandService countryCommandService): ControllerBase
+{
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateCountry(CreateCountryResource resource)
+    {
+        var createCountryCommand = CreateCountryCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var country = await countryCommandService.Handle(createCountryCommand);
+        if (country is null) return BadRequest();
+        var countryResource = CountryResourceFromEntityAssembler.ToResourceFromEntity(country);
+        //return CreatedAtAction(nameof(GetCountryById), new {countryId = countryResource.Id}, countryResource);
+        return Ok(countryResource);
+    }
+    
+    //put
+    [HttpPut("{countryId:int}")]
+    public async Task<IActionResult> UpdateCountry(int countryId, CreateCountryResource resource)
+    {
+        var updateCountryCommand = new UpdateCountryCommand(countryId, resource.Name);
+        var country = await countryCommandService.Handle(updateCountryCommand);
+        if (country == null) return NotFound();
+        var countryResource = CountryResourceFromEntityAssembler.ToResourceFromEntity(country);
+        return Ok(countryResource);
+    }
+    
+    //delete
+    [HttpDelete("{countryId:int}")]
+    public async Task<IActionResult> DeleteCountry(int countryId)
+    {
+        var deleteCountryCommand = new DeleteCountryCommand(countryId);
+        var country = await countryCommandService.Handle(deleteCountryCommand);
+        if (country == null) return NotFound();
+        var countryResource = CountryResourceFromEntityAssembler.ToResourceFromEntity(country);
+        return Ok(countryResource);
+    }
+    
+    
+}
