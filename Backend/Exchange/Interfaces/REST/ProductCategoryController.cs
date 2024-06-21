e3c27dec8c3a1fecf8623cd72f65a794441a1979
123-Mathias <u202216282@upc.edu.pe>
2024-06-21 09:31:43 -0500

    Feat: Product Controller
    In this part, we created Product Controller.

--- Backend/Exchange/Interfaces/REST/ProductCategoryController.cs
+++ Backend/Exchange/Interfaces/REST/ProductCategoryController.cs
@@ -2,6 +2,7 @@ using Backend.Exchange.Application.Internal.CommandServices;
 using Backend.Exchange.Application.Internal.QueryServices;
 using Backend.Exchange.Domain.Model.Commnads.ProductCategoryCommmands;
 using Backend.Exchange.Domain.Model.Queries.ProductCategoryQueries;
+using Backend.Exchange.Domain.Services;
 using Backend.Exchange.Interfaces.REST.Resources;
 using Backend.Exchange.Interfaces.REST.Transform;
 using Microsoft.AspNetCore.Mvc;
@@ -10,59 +11,9 @@ namespace Backend.Exchange.Interfaces.REST;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class ProductCategoryController(ProductCategoryCommandService productCategoryCommandService,ProductCategoryQueryService productCategoryQueryService) : ControllerBase
+public class ProductCategoryController(IProductCategoryCommandService productCategoryCommandService,IProductCategoryQueryService productCategoryQueryService) : ControllerBase
 {
-    /*?
-      [HttpPost]
-    public async Task<IActionResult> CreateCategory(CreateCategoryResource resource)
-    {
-        var createCategoryCommand = CreateCategoryFromResourceAssembler.ToCommandFromResource(resource);
-        var category = await categoryCommandService.Handle(createCategoryCommand);
-        if (category is null) return BadRequest();
-        var categoryResource = CategoryResourceFromEntityAssembler.ToResourceFromEntity(category);
-        return CreatedAtAction(nameof(GetCategoryById), new {categoryId = categoryResource.Id}, categoryResource);
-    }
     
-    
-    [HttpGet]
-    public async Task<IActionResult> GetAllCategories()
-    {
-        var getAllCategoriesQuery = new GetAllCategoriesQuery();
-        var categories = await categoryQueryService.Handle(getAllCategoriesQuery);
-        var categoryResources = categories.Select(CategoryResourceFromEntityAssembler.ToResourceFromEntity);
-        return Ok(categoryResources);
-    }
-    
-    [HttpGet("{categoryId:int}")]
-    public async Task<IActionResult> GetCategoryById(int categoryId)
-    {
-        var getCategoryByIdQuery = new GetCategoryByIdQuery(categoryId);
-        var category = await categoryQueryService.Handle(getCategoryByIdQuery);
-        if (category == null) return NotFound();
-        var categoryResource = CategoryResourceFromEntityAssembler.ToResourceFromEntity(category);
-        return Ok(categoryResource);
-    }
-    
-    [HttpDelete("{categoryId:int}")]
-    public async Task<IActionResult> DeleteCategory(int categoryId)
-    {
-        var deleteCategoryCommand = new DeleteCategoryCommand(categoryId);
-        var category = await categoryCommandService.Handle(deleteCategoryCommand);
-        if (category == null) return NotFound();
-        var categoryResource = CategoryResourceFromEntityAssembler.ToResourceFromEntity(category);
-        return Ok(categoryResource);
-    }
-    
-    [HttpPut("{categoryId:int}")]
-    public async Task<IActionResult> UpdateCategory(int categoryId, CreateCategoryResource resource)
-    {
-        var updateCategoryCommand = new UpdateCategoryCommand(categoryId, resource.Name);
-        var category = await categoryCommandService.Handle(updateCategoryCommand);
-        if (category == null) return NotFound();
-        var categoryResource = CategoryResourceFromEntityAssembler.ToResourceFromEntity(category);
-        return Ok(categoryResource);
-    }
-     */
     
     [HttpPost]
     public async Task<IActionResult> CreateProductCategory(CreateProductCategoryResource resource)
---
+++ Backend/Exchange/Interfaces/REST/ProductController.cs
@@ -0,0 +1,131 @@
+using Backend.Exchange.Application.Internal.CommandServices;
+using Backend.Exchange.Application.Internal.QueryServices;
+using Backend.Exchange.Domain.Model.Commnads.ProductCommands;
+using Backend.Exchange.Domain.Model.Queries.ProductQueries;
+using Backend.Exchange.Domain.Services;
+using Backend.Exchange.Interfaces.REST.Resources;
+using Backend.Exchange.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Exchange.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ProductController(IProductQueryService productQueryService,IProductCommandService productCommandService) : ControllerBase
+{
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts()
+    {
+        var getAllProductsQuery = new GetAllProductsQuery();
+        var products = await productQueryService.Handle(getAllProductsQuery);
+        var productResources = products.Select(ProductResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(productResources);
+    }
+    
+    [HttpGet("{productId:int}")]
+    public async Task<IActionResult> GetProductById(int productId)
+    {
+        var getProductByIdQuery = new GetProductByIdQuery(productId);
+        var product = await productQueryService.Handle(getProductByIdQuery);
+        if (product == null) return NotFound();
+        var productResource = ProductResourceFromEntityAssembler.ToResourceFromEntity(product);
+        return Ok(productResource);
+    }
+    
+   [HttpGet("boost/{boost:bool}")]
+   public async Task<IActionResult> GetAllAvailableProductsByBoost(bool boost)
+   {
+         var getAllAvailableProductsByBoostQuery = new GetAllAvailableProductsByBoostQuery(boost);
+         var products = await productQueryService.Handle(getAllAvailableProductsByBoostQuery);
+         var productResources = products.Select(ProductResourceFromEntityAssembler.ToResourceFromEntity);
+         return Ok(productResources);
+   }
+   
+   
+   //get by category:
+    [HttpGet("category/{categoryId:int}")]
+    public async Task<IActionResult> GetAllAvailableProductsByCategory(int categoryId)
+    {
+        var getAllAvailableProductsByCategoryQuery = new GetAllAvailableProductsByCategoryIdQuery(categoryId);
+        var products = await productQueryService.Handle(getAllAvailableProductsByCategoryQuery);
+        var productResources = products.Select(ProductResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(productResources);
+    }
+    
+    //get by word name:
+    [HttpGet("name/{name}")]
+    public async Task<IActionResult> GetAllAvailableProductsByName(string name)
+    {
+        var getAllAvailableProductsByNameQuery = new GetAllAvailableProductsByWordNameQuery(name);
+        var products = await productQueryService.Handle(getAllAvailableProductsByNameQuery);
+        var productResources = products.Select(ProductResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(productResources);
+    }
+    
+    //by user adn aviable, 2 parameters:
+    [HttpGet("user/{userId:int}/available/{available:bool}")]
+    public async Task<IActionResult> GetAllAvailableProductsByUserAndAvailable(int userId, bool available)
+    {
+        var getAllAvailableProductsByUserAndAvailableQuery = new GetAllProductsByUserAndAvailableQuery(userId, available);
+        var products = await productQueryService.Handle(getAllAvailableProductsByUserAndAvailableQuery);
+        var productResources = products.Select(ProductResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(productResources);
+    }
+    
+    //by price, district, word name and category:
+    [HttpGet("price/{minPrice:float}/{maxPrice:float}/district/{districtId:int}/name/{name}/category/{categoryId:int}")]
+    public async Task<IActionResult> GetAllAvailableProductsByBetweenPricesAndDistrictAndWordNameAndCategory(int minPrice, int maxPrice, int districtId, string name, int categoryId)
+    {
+        var getAllAvailableProductsByBetweenPricesAndDistrictAndWordNameAndCategoryQuery = 
+            new GetAllAvailableProductsByBetweenPricesAndDistrictAndWordNameAndCategoryQuery(minPrice, maxPrice, districtId, name, categoryId);
+        
+        var products = await productQueryService.Handle(getAllAvailableProductsByBetweenPricesAndDistrictAndWordNameAndCategoryQuery);
+        var productResources = products.Select(ProductResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(productResources);
+    }
+    
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(CreateProductResource resource)
+    {
+        var createProductCommand = CreateProductCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var product = await productCommandService.Handle(createProductCommand);
+        if (product is null) return BadRequest();
+        var productResource = ProductResourceFromEntityAssembler.ToResourceFromEntity(product);
+        return CreatedAtAction(nameof(GetProductById), new {productId = productResource.Id}, productResource);
+    }
+    
+    
+    [HttpPut("{productId:int}")]
+    public async Task<IActionResult> UpdateProduct(int productId, CreateProductResource resource)
+    {
+        var updateProductCommand = new UpdateProductCommand(productId, resource.Name, resource.Description,
+            resource.ObjectChange, resource.Price, resource.Photo, resource.Boost, resource.Available,
+            resource.CategoryId, resource.UserId, resource.DistrictId);
+        
+        var product = await productCommandService.Handle(updateProductCommand);
+        if (product is null) return NotFound();
+        var productResource = ProductResourceFromEntityAssembler.ToResourceFromEntity(product);
+        return Ok(productResource);
+    }
+    
+    [HttpDelete("{productId:int}")]
+    public async Task<IActionResult> DeleteProduct(int productId)
+    {
+        var deleteProductCommand = new DeleteProductCommand(productId);
+        var product = await productCommandService.Handle(deleteProductCommand);
+        if (product is null) return NotFound();
+        var productResource = ProductResourceFromEntityAssembler.ToResourceFromEntity(product);
+        return Ok(productResource);
+    }
+    
+    
+   
+   
+    
+    
+    
+
+    
+}
