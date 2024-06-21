83f220e5dcec8eb04926d401a8fe27ef99022a30
123-Mathias <u202216282@upc.edu.pe>
2024-06-21 09:11:23 -0500

    Feat: Query Service District
    In this part, we created Query Service District.

---
+++ Backend/Exchange/Domain/Services/IDistrictQueryService.cs
@@ -0,0 +1,11 @@
+using Backend.Donation.Domain.Model.Queries.Category;
+using Backend.Exchange.Domain.Model.Aggregates;
+using Backend.Exchange.Domain.Model.Queries.DistrictQueries;
+
+namespace Backend.Exchange.Domain.Services;
+
+public interface IDistrictQueryService
+{
+    Task<IEnumerable<District>> Handle(GetAllDistrictsQuery query);
+    Task<District?> Handle(GetDistrictByIdQuery query);
+}
