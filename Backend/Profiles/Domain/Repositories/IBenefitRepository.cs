f84d5d4b3e1d27826331b78f9f1e61fec57001a2
IanHD04 <u202214059@upc.edu.pe>
2024-06-22 09:23:47 -0500

    Feat: Interface Membership Repository
    In this part, we created membership respository.

---
+++ Backend/Profiles/Domain/Repositories/IBenefitRepository.cs
@@ -0,0 +1,9 @@
+using Backend.Profiles.Domain.Model.Aggregates;
+using Backend.Shared.Domain.Repositories;
+
+namespace Backend.Profiles.Domain.Repositories;
+
+public interface IBenefitRepository : IBaseRepository<Benefit>
+{
+    
+}
